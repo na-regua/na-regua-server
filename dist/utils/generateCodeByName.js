f0cc9b5286855b473d52f96510b20c0cfd0be144
Alex Rodrigues <alex.rodrigues@venturus.org.br>
2023-11-15 12:46:51 -0300

    ci/cd: remove dist from main branch

--- dist/utils/generateCodeByName.js
+++
@@ -1,1 +0,0 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});exports.generateCodeByName=void 0;function generateCodeByName(){return Math.floor(Math.random()*167772151212).toString(16).toLocaleUpperCase();}exports.generateCodeByName=generateCodeByName;
